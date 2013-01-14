8b64f2200076586c0297cfd8a1a952de613b9e7f
Dan Schlossberg <danschlossberg@gmail.com>
2013-01-14 10:42:34 -0500

    fix references

--- SimpleMembership._Tests/MxClient_Tests.cs
+++ SimpleMembership._Tests/MxClient_Tests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Web.WebPages.OAuth;
 using NUnit.Framework;
 
 namespace SimpleMembership._Tests.Controllers
