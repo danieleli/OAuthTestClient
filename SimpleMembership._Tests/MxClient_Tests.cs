dbabcad09467b8fbb8a9191b84b60b7e91c3efb6
Dan Schlossberg <danschlossberg@gmail.com>
2013-01-14 16:03:10 -0500

    added references for tests

--- SimpleMembership._Tests/MxClient_Tests.cs
+++ SimpleMembership._Tests/MxClient_Tests.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Web.WebPages.OAuth;
 using NUnit.Framework;
+using Rhino.Mocks;
+using SimpleMembership.Auth.OAuth1a;
 
 namespace SimpleMembership._Tests.Controllers
 {
@@ -13,8 +17,18 @@ namespace SimpleMembership._Tests.Controllers
         [Test]
         public void Test1()
         {
-            //TestAuthConfig.RegisterAuth();
-            //OAuthWebSecurity.Login("MxMerchant", providerUserId: "", createPersistentCookie: false);
+            TestAuthConfig.RegisterAuth();
+            OAuthWebSecurity.Login("MxMerchant", providerUserId: "", createPersistentCookie: false);
+
+        }
+
+        [Test]
+        public void TokenManager()
+        {
+            var mxClient = new MxClient("a","b");
+            Assert.NotNull(mxClient, "mxClient");
+            var tokens = mxClient.TokenManager;
+            Assert.NotNull(tokens, "tokens");
 
         }
     }
