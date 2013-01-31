6f359c058f5408ab52bd75df45b12aba4d7a5916
Dan Schlossberg <danschlossberg@gmail.com>
2013-01-31 16:27:06 -0500

    move uri test to new test class

---
+++ SS.OAuth1.Client._Tests/Tests/Extensions/UriExTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using NUnit.Framework;
+using SS.OAuth1.Client.Extensions;
+using SS.OAuth1.Client.Helpers;
+using SS.OAuth1.Client.Parameters;
+using log4net;
+
+namespace SS.OAuth1.Client._Tests.Tests.Helpers
+{
+    [TestFixture]
+    public class UriExTest
+    {
+        private static readonly ILog LOG = LogManager.GetLogger(typeof(UriExTest));
+
+        private const string SSL_URL = "HTTPS://WWW.EXAMPLE.COM/ROUT1/ROUT2?NAME=SOMENAME&ZIP=20303";
+        private readonly Uri _uri = new Uri(SSL_URL);
+
+        [Test]
+        public void GetBaseStringUri_Returns_LowerCaseScheme()
+        {
+            var result = UriEx.GetBaseStringUri(_uri);
+
+            LOG.Debug("result: " + result);
+            Assert.That(result, Is.StringStarting("https"), "scheme");
+
+        }
+
+        [Test]
+        public void GetBaseStringUri_Returns_LowerCaseHost()
+        {
+            var result = UriEx.GetBaseStringUri(_uri);
+
+            LOG.Debug("result: " + result);
+            Assert.That(result, Is.StringEnding("www.example.com"), "host");
+        }
+
+        [Test]
+        public void GetEncodedBaseUri_Encodes_Slashes()
+        {
+            Assert.That(_uri.ToString, Contains.Substring("/"), "Slash");
+
+            var result = UriEx.GetBaseStringUri(_uri).UrlEncodeForOAuth();
+
+            LOG.Debug("result: " + result);
+            Assert.That(result, Is.Not.ContainsSubstring("/"), "Slash");
+        }
+
+
+        [Test]
+        public void GetEncodedBaseUri_Encodes_Colon()
+        {
+            Assert.That(_uri.ToString, Contains.Substring(":"), "Colon");
+
+            var result = UriEx.GetBaseStringUri(_uri).UrlEncodeForOAuth();
+
+            Assert.That(result, Is.Not.ContainsSubstring(":"), "Colon");
+        }
+
+    }
+}
--- SS.OAuth1.Client._Tests/Tests/Helpers/OAuthParserTest.cs
+++ SS.OAuth1.Client._Tests/Tests/Helpers/OAuthParserTest.cs
@@ -32,36 +32,5 @@ namespace SS.OAuth1.Client._Tests.Tests.Helpers
             Assert.That(header, Is.Not.ContainsSubstring(secret), "consumer secret");
         }
 
-        private const string SIMPLE_URL = "HTTP://WWW.EXAMPLE.COM/ROUT1/ROUT2?NAME=SOMENAME&ZIP=20303";
-        private const string SSL_URL = "HTTPS://WWW.EXAMPLE.COM/ROUT1/ROUT2?NAME=SOMENAME&ZIP=20303";
-        
-        [Test]
-        public void GetBaseStringUri_Returns_LowerCaseScheme()
-        {
-
-            var uri = new Uri(SSL_URL);
-
-            var result = UriEx.GetBaseStringUri(uri);
-
-
-            LOG.Debug("result: " + result);
-            Assert.That(result, Is.StringStarting("https"), "scheme");
-
-        }
-
-        [Test]
-        public void GetBaseStringUri_Returns_LowerCaseHost()
-        {
-
-            var uri = new Uri(SSL_URL);
-
-            var result = UriEx.GetBaseStringUri(uri);
-
-
-            LOG.Debug("result: " + result);
-            Assert.That(result, Is.StringEnding("www.example.com"), "host");
-
-        }
-
     }
 }
