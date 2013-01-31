01df6cfddad727658c65f9edaa7ada531ffccfbc
Dan Schlossberg <danschlossberg@gmail.com>
2013-01-31 17:31:19 -0500

    tests for oauth normalized params

--- SS.OAuth1.Client._Tests/Tests/Helpers/NormalizedParametersTest.cs
+++ SS.OAuth1.Client._Tests/Tests/Helpers/NormalizedParametersTest.cs
@@ -33,39 +33,39 @@ namespace SS.OAuth1.Client._Tests.Tests.Helpers
         [Test]
         public void NormalizedRequestParameters_Include_QueryStringParams()
         {
-            var normalizedRequestParams = GetNormalizedParams(_testParam);
+            var normalizedRequestParams = GetNormalizedRequestParameters(_testParam);
 
             LOG.Debug("normalizedRequestParams: " + normalizedRequestParams);
 
             Assert.That(normalizedRequestParams, Is.Not.Null, "normalizedRequestParams");
 
-            var valuesUnderKeyA = normalizedRequestParams.GetValues("a");
-            Assert.That(valuesUnderKeyA, Is.Not.Null, "valuesUnderKeyA");
-            Assert.That(valuesUnderKeyA.Length, Is.EqualTo(1), "Count of RequestParams Named 'a'");
+            var valuesA = normalizedRequestParams.GetValues("a");
+            Assert.That(valuesA, Is.Not.Null, "valuesUnderKeyA");
+            Assert.That(valuesA.Length, Is.EqualTo(1), "Count of RequestParams Named 'a'");
 
 
-            var valuesUnderKeyB = normalizedRequestParams.GetValues("b");
-            Assert.That(valuesUnderKeyB, Is.Not.Null, "valuesUnderKeyB");
-            Assert.That(valuesUnderKeyB.Length, Is.EqualTo(2), "Count of RequestParams Named 'b'");
+            var valuesB = normalizedRequestParams.GetValues("b");
+            Assert.That(valuesB, Is.Not.Null, "valuesUnderKeyB");
+            Assert.That(valuesB.Length, Is.EqualTo(2), "Count of RequestParams Named 'b'");
         }
 
         [Test]
         public void NormalizedRequestParameters_Includes_ItemWithName_ConsumerKey()
         {
-            var normalizedRequestParams = GetNormalizedParams(_testParam);
+            var normalizedRequestParams = GetNormalizedRequestParameters(_testParam);
 
             LOG.Debug("normalizedRequestParams: " + normalizedRequestParams);
 
-            var oauthConsumerTokenKeyValues = normalizedRequestParams.GetValues(Keys.CONSUMER_KEY);
-            Assert.That(oauthConsumerTokenKeyValues, Is.Not.Null, "oauthConsumerTokenKey_Values");
-            Assert.That(oauthConsumerTokenKeyValues.Length, Is.EqualTo(1), "Count of RequestParams Named " + Keys.CONSUMER_KEY);
+            var values = normalizedRequestParams.GetValues(Keys.CONSUMER_KEY);
+            Assert.That(values, Is.Not.Null, "oauthConsumerTokenKey_Values");
+            Assert.That(values.Length, Is.EqualTo(1), "Count of RequestParams Named " + Keys.CONSUMER_KEY);
 
 
             LOG.Debug("normalizedRequestParams.Stringify: " + normalizedRequestParams.Stringify());
 
         }
 
-        public NameValueCollection GetNormalizedParams(TestParameter p)
+        public NameValueCollection GetNormalizedRequestParameters(TestParameter p)
         {
             var rtnCollection = p.RequestUri.ParseQueryString();
             
