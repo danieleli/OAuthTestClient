89f5fcf9db955c608b7a1d9f7fd43d9c05b4747c
Dan Schlossberg <danschlossberg@gmail.com>
2013-01-31 16:16:01 -0500

    Add OAuth2 library and test library

---
+++ SS.OAuth2.Client._Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS.OAuth2.Client._Tests
+{
+    public class Class1
+    {
+    }
+}
